77e520a29446ff0d7efe96ffed15b1f488f33db2
Hijirichan <floremilfia@hotmail.com>
2014-05-10 21:02:18 +0900

    I have changed!!
    ロゴは作れないけれどこういうことは出来ます。

--- hello_world.php
+++ hello_world.php
@@ -1,6 +1,16 @@
 <?php
-  echo "Good Bye!";
-  echo "Hello again!?";
-  echo "Hello by coa";
-  echo "Hello by mori";
- ?>
+
+  // Hello, 名前を表示する
+  function SayHello(name){
+    print "Hello, $name.\r\n";
+  }
+
+  // echoよりprint文の方が良い
+  print "Good Bye!\r\n";
+  print "Hello again!?\r\n";
+  
+  // こんな感じに使う
+  SayHello("coa");
+  SayHello("mori");
+
+?>
